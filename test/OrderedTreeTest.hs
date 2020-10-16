0b889b930386fe600a97629c3d0e51bd1a8f741d
Davide Fauri <davide.fauri@gmail.com>
2020-10-16 18:38:34 +0200

    add tests for OrderedTree

--- test/OrderedTreeTest.hs
+++ test/OrderedTreeTest.hs
@@ -1,10 +1,48 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module OrderedTreeTest (testOrderedTree) where
 
-import OrderedTree
+import OrderedTree (OrderedTree, insert, inorder, fromList)
+import Data.List (sort)
 import Test.Tasty
-import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 
+-- UTILS
+
+newtype AnyOrderedTree a = AnyOrderedTree (OrderedTree a) deriving (Show)
+
+instance (Ord a, Arbitrary a) => Arbitrary (AnyOrderedTree a) where
+  arbitrary =  (AnyOrderedTree . fromList) <$> listOf arbitrary
+
+isOrdered :: Ord a => OrderedTree a -> Bool
+isOrdered t = let traversal = inorder t
+  in traversal == sort traversal
+
+-- TESTS
+
 testOrderedTree :: TestTree
-testOrderedTree = testGroup "Ordered Tree" $
-  [testCase "TODO" $ True @?= True]
+testOrderedTree = testGroup "Ordered Tree" [ testOrderedGeneration
+                                           , testOrderedInsert
+                                           , testOrderedMerge
+                                           ]
+
+testOrderedGeneration :: TestTree
+testOrderedGeneration = testProperty "generates correctly" $
+  (propOrderedGeneration :: OrderedList Int -> Bool)
+    where
+      propOrderedGeneration :: (Ord a) => OrderedList a -> Bool
+      propOrderedGeneration = isOrdered . fromList . getOrdered
+
+testOrderedInsert :: TestTree
+testOrderedInsert = testProperty "preserves ordering on inserts" $
+  (propOrderedInsert :: Int -> AnyOrderedTree Int -> Bool)
+    where
+      propOrderedInsert :: (Ord a) => a -> AnyOrderedTree a -> Bool
+      propOrderedInsert el (AnyOrderedTree t) = isOrdered (insert el t)
+
+testOrderedMerge :: TestTree
+testOrderedMerge = testProperty "preserves ordering on merges" $
+  (propOrderedMerge :: AnyOrderedTree Int -> AnyOrderedTree Int -> Bool)
+    where
+      propOrderedMerge ::(Ord a) => AnyOrderedTree a -> AnyOrderedTree a -> Bool
+      propOrderedMerge (AnyOrderedTree t1) (AnyOrderedTree t2) = isOrdered (t1 <> t2)
+
--- test/Test.hs
+++ test/Test.hs
@@ -1,10 +1,12 @@
 import Test.Tasty
 
 import TreeTest (testTree)
+import OrderedTreeTest (testOrderedTree)
 
 main :: IO ()
 main = defaultMain $ allTests
 
 allTests :: TestTree
 allTests = testGroup "" $ [ testTree
+                          , testOrderedTree
                           ]
